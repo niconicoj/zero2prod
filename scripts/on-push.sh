73ec9613ac48d466eed73a005d46524c6266cea6
Nicolas JOULIN <joulin.nicolas@gmail.com>
2023-11-25 11:32:51 +0100

    feat: update push hook

---
+++ scripts/on-push.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Function to check if a command is available
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Check if sqlx-cli is installed, if not, install it
+if ! command_exists sqlx; then
+    echo "sqlx-cli not found. Installing..."
+    cargo install sqlx-cli
+fi
+
+# Check if sqlx.json is valid
+if ! cargo sqlx prepare --workspace --check; then
+    exit 1
+fi
+
+# Run the test suite
+echo "Running tests..."
+cargo test
+
+# Check if tests passed
+if [ $? -ne 0 ]; then
+    echo "Error: Test suite failed."
+    exit 1
+fi
+
+echo "Pre-commit checks passed."
+exit 0
