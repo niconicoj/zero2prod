7c6b5197b5b9089463eca3ad70b5ad8a4f0a5d2b
Nicolas JOULIN <joulin.nicolas@gmail.com>
2023-11-26 12:14:39 +0100

    fix: db name

--- init_db.sh
+++ init_db.sh
@@ -20,7 +20,7 @@ DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
-DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_NAME="${POSTGRES_DB:=stomp-db}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
